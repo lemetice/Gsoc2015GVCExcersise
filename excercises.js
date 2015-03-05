76c2ee0162cf4227c9872ef85711be567b098800
lemetice <arnoldtagne@gmail.com>
2015-03-05 06:32:35 +0100

    commit all files

---
+++ excercises.js
@@ -0,0 +1,17 @@
+/*************************************
+	Gsoc assigments
+**************************************/
+
+//Excercise 1
+
+
+
+function palindrome(){
+	var string_provided = prompt("Please enter a string", "Provide a string");
+	if(string_provided != null && string_provided != ""){
+		var array={};
+		
+		alert('Helloooo');
+	}
+
+}
