097d59a7a5b4ae85add7ec5cd35f3f4a56a1c2ba
lemetice <arnoldtagne@gmail.com>
2015-03-07 11:33:50 +0100

    exercise 1 done

--- excercises.js
+++ excercises.js
@@ -8,11 +8,33 @@
 
 function palindrome(){
 	var string_provided = prompt("Please enter a string", "Provide a string");
+
 	if(string_provided != null && string_provided != ""){
-		var array={};
 		
-		alert('Helloooo');
+		reversed_str = string_provided.split('').reverse().join('');
+		alert(reversed_str);
+		if(reversed_str === string_provided){
+			console.log("The word entered is a palindrome");
+			return true;
+		}
+	}
+	else{
+		console.log('Please try again and type something');
+		return null;
 	}
 	//Everything being constant
-	
+
+}
+
+
+function evenFibonacciNumber(){
+	var f={};
+	 f[0] = 0;
+	 f[1] = 1;
+
+	for(var i=2; i<10; i++){
+		f[i] = f[i-1] + f[i-2];
+		console.log(f[i]);
+	}
+
 }
